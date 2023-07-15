de8934d4d41326065d1cc79ae9e61d64423d58b4
Dilsara <janinduabeygunawardhana@gmail.com>
2023-07-15 06:00:07 +0530

    fixed vibrations

--- Mecha.ino
+++ Mecha.ino
@@ -1,73 +1,10 @@
 const byte interruptPin = 2;
-
-const uint8_t SensorCount = 6;
-uint16_t sensorValues[SensorCount];
 bool isSafe = true;
 
 int stepPin = 3;
 int directionPin = 4;
 
-void setup()
-{ 
-  Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), StepperLimitChanged, CHANGE);
-  
-  pinMode(stepPin, OUTPUT);
-  pinMode(directionPin, OUTPUT);
-  pinMode(2,INPUT);
-
-
-}
-
-void loop()
-{
-  if(isSafe){
-    Serial.println("Running");
-    liftDown(1000);  
-  }
-  else{
-    liftUp(400);
-  }
-  
-  //liftDown(10);
-  delay(100);
-  
-}
-
-
-void liftUp(int steps){
-  digitalWrite(directionPin,HIGH);
-  for (int stepCount = 1; stepCount < steps; stepCount++){
-    if(isSafe){
-      break;  
-    }
-    Serial.println("Lifting Up");
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
-  }   
-}
-
-
-int liftDown(int steps){
-  digitalWrite(directionPin,LOW);
-  for (int stepCount = 1; stepCount < steps; stepCount++){
-    if(!isSafe){
-      break;  
-    }
-    Serial.println("Lifting Down");
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
-  }   
-}
-
-
-
-void StepperLimitChanged()
-{
+void StepperLimitChanged(){
   if (digitalRead(2)){
     Serial.print("pressed , ");
     Serial.print(digitalRead(2));
@@ -82,4 +19,60 @@ void StepperLimitChanged()
     Serial.print(" Is Safe : ");
     Serial.println(isSafe);  
   }
+}
+
+void setup() {
+  attachInterrupt(digitalPinToInterrupt(interruptPin), StepperLimitChanged, CHANGE);
+  Serial.begin(9600);
+  pinMode(stepPin, OUTPUT);
+  pinMode(directionPin, OUTPUT);
+  pinMode(2,INPUT);
+
+  CalibrateLifter();
+
+}
+
+void loop() {
+  while (Serial.available() == 0) {
+  }
+  int stp = Serial.parseInt();
+
+  liftUp(stp);  
+  
+  delay(1000);
+  liftDown(stp); 
+  delay(1000);
+}
+
+void liftUp(int stp){
+  digitalWrite(directionPin, HIGH);
+
+  for (int stepCount = 1; stepCount < stp; stepCount++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+}
+
+void liftDown(int stp){
+  digitalWrite(directionPin, LOW);
+
+  for (int stepCount = 1; stepCount < stp; stepCount++){
+    if(!isSafe){
+      break;  
+    }
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+}
+
+void CalibrateLifter(){
+  liftUp(400);
+  while(isSafe){
+    liftDown(5000);
+  }
+ Serial.println("Calibration Successfull");
 }
